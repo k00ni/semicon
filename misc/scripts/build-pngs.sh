b173cbb9d766dbba838d6713fedffb33cbaa259a
Konrad Abicht <k.abicht@googlemail.com>
2012-12-07 10:21:13 +0100

    Add PNG's for namespace SVG's and add script for automatically build

---
+++ misc/scripts/build-pngs.sh
@@ -0,0 +1,15 @@
+#
+# BUILD NAMESPACE ICONS 
+#
+
+# Set folder paths
+svgFolder="$PWD/../svg/namespace"
+pngFolder="$PWD/../png/namespace"
+
+# first convert svg's to png
+for i in `find $svgFolder -depth -name '*.svg'`; do rsvg-convert $i -o `echo $i | sed -e 's/svg$/png/'`; done
+# after that move files
+for i in `find $svgFolder -depth -name '*.png'`; do 
+    filename=$(basename "$i") extension="${filename##*.}" filename="${filename%.*}"
+    mv "$svgFolder/$filename.$extension" "$pngFolder/$filename.$extension" # move files from svg to png folder
+done
