69aa2a30dbedf5a0edbe0a9b7e5c32e942ab2ffe
Konrad Abicht <k.abicht@googlemail.com>
2012-12-16 15:25:03 +0100

    Add script to update README.md with new generated PNG's

---
+++ misc/scripts/update-README.sh
@@ -0,0 +1,33 @@
+#
+# UPDATE README
+#
+
+# Reset readme
+rm ../README.md
+echo "# Semicon \n" > ../README.md
+
+# Set folder paths
+pngFolder="$PWD/../png/all"
+
+filePath="![](https://raw.github.com/k00ni/semicon/master/png/all/"
+
+# first convert svg's to png
+for i in `find $pngFolder -depth -name '*.png'`; do 
+    filename=$(basename "$i") extension="${filename##*.}" filename="${filename%.*}"
+    echo "$filePath$filename.png)" >> ../README.md;
+done
+
+echo "\n
+Semicon is a collection of icons (called semicons) for the [Semantic Web](http://en.wikipedia.org/wiki/Semantic_Web).
+There are semicons for a various topics, e.g. file or structure related ones.
+
+## Getting started
+
+Simply download it the complete icon set as [zip](https://github.com/k00ni/semicon/raw/master/downloads/semicon.zip) 
+or [tar gz](https://github.com/k00ni/semicon/raw/master/downloads/semicon.tar.gz). 
+Extract downloaded archive into your images folder and enjoy!
+
+## License
+Semicon is dual-licensed: [GPL 2](http://www.gnu.org/licenses/gpl-2.0.html) and 
+[Create Commons \"CC BY-SA\" 3.0](http://creativecommons.org/licenses/by-sa/3.0/deed.en_US), 
+which includes usage for commercial projects too." >> ../README.md
