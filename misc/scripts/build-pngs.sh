335107b0d4dde15b3315ac8cb7ebdf80bb02e9a9
rvesse <rvesse@dotnetrdf.org>
2013-02-06 15:11:39 -0800

    Reorganize the PNG conversion script to make it more maintable and portable
    This commit rewrites the build-pngs.sh script to make a number of improvements.
    
    Firstly it allows the script to choose a different conversion command depending
    on the users environment.  It now supports either using rsvg-convert directly
    or using ImageMagick convert.  rsvg-convert is used directly in preference of
    convert.
    
    Secondly it refactors the script to make the conversion a function and call that
    function several times.  This is more readable and allows for easily adding
    conversion of new folders with a single line change to the script.

--- misc/scripts/build-pngs.sh
+++ misc/scripts/build-pngs.sh
@@ -2,62 +2,74 @@
 # BUILD NAMESPACE ICONS 
 #
 
-# Set folder paths
-svgFolder="$PWD/../svg/file"
-pngFolder="$PWD/../png/file"
+# Figure out what conversion tool to use
+# Currently either rsvg-convert or convert (ImageMagick) are supported
+set +e
 
-# first convert svg's to png
-for i in `find $svgFolder -depth -name '*.svg'`; do rsvg-convert $i -o `echo $i | sed -e 's/svg$/png/'`; done
-# after that move files
-for i in `find $svgFolder -depth -name '*.png'`; do 
-    filename=$(basename "$i") extension="${filename##*.}" filename="${filename%.*}"
-    mv "$svgFolder/$filename.$extension" "$pngFolder/$filename.$extension" # move files from svg to png folder
-done
+convertCmd=
+convertArgs=
 
-# Set folder paths
-svgFolder="$PWD/../svg/misc"
-pngFolder="$PWD/../png/misc"
+# Try rsvg-convert first
+which rsvg-convert >> /dev/null
+if [ $? -eq 0 ]; then
+    convertCmd="rsvg-convert";
+    convertArgs="-o"
+else
+    # Try ImageMagick convert second
+    which convert >> /dev/null
+    if [ $? -eq 0 ]; then
+	# ImageMagick is installed, does it support svg input?
+	convert -list delegate | grep "svg =" >> /dev/null
+	if [ $? -eq 0 ]; then
+	        convertCmd="convert"
+	else
+		# convert installed but no SVG support
+		echo "Error - ImageMagick convert is installed but no SVG support appears to be installed"
+		exit 1
+	fi
+    else
+	# Neither rsvg-convert or ImageMagick convert are installed
+        echo "Error - No suitable convert utility available, install rsvg-convert or ImageMagick convert"
+        exit 1
+    fi
+fi
+set -e
+convertCmdPath=`which $convertCmd`
+echo "Using conversion utility $convertCmd from $convertCmdPath"
 
-# first convert svg's to png
-for i in `find $svgFolder -depth -name '*.svg'`; do rsvg-convert $i -o `echo $i | sed -e 's/svg$/png/'`; done
-# after that move files
-for i in `find $svgFolder -depth -name '*.png'`; do 
-    filename=$(basename "$i") extension="${filename##*.}" filename="${filename%.*}"
-    mv "$svgFolder/$filename.$extension" "$pngFolder/$filename.$extension" # move files from svg to png folder
-done
+# Conversion Function
+svgToPng() {
+    svgFolder=$1
+    pngFolder=$2
 
-# Set folder paths
-svgFolder="$PWD/../svg/structure"
-pngFolder="$PWD/../png/structure"
+    echo "Converting from SVGs in directory $svgFolder to PNGs in directory $pngFolder"
 
-# first convert svg's to png
-for i in `find $svgFolder -depth -name '*.svg'`; do rsvg-convert $i -o `echo $i | sed -e 's/svg$/png/'`; done
-# after that move files
-for i in `find $svgFolder -depth -name '*.png'`; do 
-    filename=$(basename "$i") extension="${filename##*.}" filename="${filename%.*}"
-    mv "$svgFolder/$filename.$extension" "$pngFolder/$filename.$extension" # move files from svg to png folder
-done
+    # First convert svg's to png
+    for i in `find $svgFolder -depth -name '*.svg'`; do
+        filename=`echo $i | sed -e 's/svg$/png/'`
+        $convertCmd "$i" $convertArgs "$filename";
+    done
 
-# Set folder paths
-svgFolder="$PWD/../svg/terms"
-pngFolder="$PWD/../png/terms"
+    # After that move files
+    for i in `find $svgFolder -depth -name '*.png'`; do 
+        filename=$(basename "$i") 
+        extension="${filename##*.}"
+        filename="${filename%.*}"
+        # Move files from svg to png folder
+        mv "$svgFolder/$filename.$extension" "$pngFolder/$filename.$extension"
+    done
+}
 
-# first convert svg's to png
-for i in `find $svgFolder -depth -name '*.svg'`; do rsvg-convert $i -o `echo $i | sed -e 's/svg$/png/'`; done
-# after that move files
-for i in `find $svgFolder -depth -name '*.png'`; do 
-    filename=$(basename "$i") extension="${filename##*.}" filename="${filename%.*}"
-    mv "$svgFolder/$filename.$extension" "$pngFolder/$filename.$extension" # move files from svg to png folder
-done
+# Convert the various icon sets
 
-# Set folder paths
-svgFolder="$PWD/../svg/triplestore"
-pngFolder="$PWD/../png/triplestore"
+# Structure Icons
+svgToPng "$PWD/../svg/structure" "$PWD/../png/structure"
 
-# first convert svg's to png
-for i in `find $svgFolder -depth -name '*.svg'`; do rsvg-convert $i -o `echo $i | sed -e 's/svg$/png/'`; done
-# after that move files
-for i in `find $svgFolder -depth -name '*.png'`; do 
-    filename=$(basename "$i") extension="${filename##*.}" filename="${filename%.*}"
-    mv "$svgFolder/$filename.$extension" "$pngFolder/$filename.$extension" # move files from svg to png folder
-done
+# Terms Icons
+svgToPng "$PWD/../svg/terms" "$PWD/../png/terms"
+
+# Triplestore Icons
+svgToPng "$PWD/../svg/triplestore" "$PWD/../png/triplestore"
+
+echo "Icon Conversion completed"
+
