a9c111b712f83e7954dfc16bdc94806b7ed6f31f
rvesse <rvesse@dotnetrdf.org>
2013-02-06 15:34:44 -0800

    Add support for generating PNGs in the desired size to the build-pngs.sh script
    This commit adds an optional argument that may be passed to build-pngs.sh
    which will cause PNGs of the specified size to be generated.  Default
    size generated remains at 16x16

--- misc/scripts/build-pngs.sh
+++ misc/scripts/build-pngs.sh
@@ -2,6 +2,11 @@
 # BUILD NAMESPACE ICONS 
 #
 
+# What size PNGs are we generating?
+size=$1
+size=${size:=16}
+echo "Generating PNGs of ${size}x${size} pixels"
+
 # Figure out what conversion tool to use
 # Currently either rsvg-convert or convert (ImageMagick) are supported
 set +e
@@ -13,7 +18,7 @@ convertArgs=
 which rsvg-convert >> /dev/null
 if [ $? -eq 0 ]; then
     convertCmd="rsvg-convert";
-    convertArgs="-o"
+    convertArgs="-w $size -h ${size} -o"
 else
     # Try ImageMagick convert second
     which convert >> /dev/null
@@ -22,6 +27,7 @@ else
 	convert -list delegate | grep "svg =" >> /dev/null
 	if [ $? -eq 0 ]; then
 	        convertCmd="convert"
+		convertArgs="-resize ${size}x${size}"
 	else
 		# convert installed but no SVG support
 		echo "Error - ImageMagick convert is installed but no SVG support appears to be installed"
@@ -47,8 +53,13 @@ svgToPng() {
     # First convert svg's to png
     for i in `find $svgFolder -depth -name '*.svg'`; do
         filename=`echo $i | sed -e 's/svg$/png/'`
-        $convertCmd "$i" $convertArgs "$filename";
+	$convertCmd "$i" $convertArgs "$filename"
     done
+
+    # May need to create destination directory
+    if [ ! -d $pngFolder ]; then
+	mkdir $pngFolder
+    fi
 
     # After that move files
     for i in `find $svgFolder -depth -name '*.png'`; do 
@@ -62,14 +73,19 @@ svgToPng() {
 
 # Convert the various icon sets
 
+sizeDir=
+if [ ! ${size} -eq 16 ]; then
+    sizeDir="/$size"
+fi
+
 # Structure Icons
-svgToPng "$PWD/../svg/structure" "$PWD/../png/structure"
+svgToPng "$PWD/../svg/structure" "$PWD/../png/structure$sizeDir"
 
 # Terms Icons
-svgToPng "$PWD/../svg/terms" "$PWD/../png/terms"
+svgToPng "$PWD/../svg/terms" "$PWD/../png/terms$sizeDir"
 
 # Triplestore Icons
-svgToPng "$PWD/../svg/triplestore" "$PWD/../png/triplestore"
+svgToPng "$PWD/../svg/triplestore" "$PWD/../png/triplestore$sizeDir"
 
 echo "Icon Conversion completed"
 
